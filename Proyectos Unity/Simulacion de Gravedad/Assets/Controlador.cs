e99b891279a2bfe6e10c3f9be601443cbc981bc0
Bitmasking <78149868+Bitmasking@users.noreply.github.com>
2021-05-29 21:10:07 -0500

    Add files via upload

---
+++ Proyectos Unity/Simulacion de Gravedad/Assets/Controlador.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controlador : MonoBehaviour
+{
+    public GameObject uwu;
+    public Rigidbody rb;
+    const float G = 667.4f;
+    public static List<Controlador> controllers;
+    private void FixedUpdate()
+    {
+        foreach (Controlador item in controllers)
+        {
+            if (item != this)
+                Gravedad(item);
+        }
+    }
+    void Update()
+    {
+        if (uwu.transform.position.x > 4500 || uwu.transform.position.x < -4500 || uwu.transform.position.y > 2600 || uwu.transform.position.y < -2600)
+        {
+            Destroy(uwu);
+        }
+    }
+    void OnEnable()
+    {
+        if (controllers == null) controllers = new List<Controlador>();
+        controllers.Add(this);
+    }
+    void OnDisable()
+    {
+        controllers.Remove(this);
+    }
+
+    void Gravedad(Controlador other)
+    {
+        Rigidbody rbother = other.rb;
+        Vector3 dir = rb.position - rbother.position;
+        float dis = dir.magnitude;
+        if (dis == 0f) return;
+        float mg = G * (rb.mass * rbother.mass) / Mathf.Pow(dis, 2);
+        Vector3 fuerza = dir.normalized * mg;
+        rbother.AddForce(fuerza);
+    }
+}
---
+++ Proyectos Unity/Simulacion de Gravedad/Assets/Generar.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generar : MonoBehaviour
+{
+    public int gen = 10;
+    public GameObject prefab;
+    List<GameObject> prefabs;
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            print("RESET");
+            prefabs = new List<GameObject>();
+            gen = Random.Range(10, 20);
+            print(gen);
+            for (int i = 0; i < gen; i++)
+            {
+                int x = Random.Range(-1500, 1500);
+                int y = Random.Range(-1500, 1500);
+                prefabs.Add(Instantiate(prefab, new Vector3(x, y, 0), Quaternion.identity));
+                int uwu = Random.Range(200, 500);
+                prefabs[i].transform.localScale = new Vector3(uwu, uwu, uwu);
+                prefabs[i].GetComponent<Rigidbody>().mass = uwu * 500;
+                float r = Random.Range(10f, 255f);
+                float g = Random.Range(10f, 255f);
+                float b = Random.Range(10f, 255f);
+                Color col = new Vector4(r / 255f, g / 255f, b / 255f, 0);
+                prefabs[i].GetComponent<Renderer>().material.SetColor("_EmissionColor", col);
+            }
+        }
+
+    }
+
+}
